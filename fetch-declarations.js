a26113a3d57fcabcd60aa8afc4d320d4e27def8c
Maksym Popenko <max@cemoe.com>
2016-05-25 00:53:14 +0300

    Barebone of readGoogleSheetsBunchFromFile functionality

---
+++ fetch-declarations.js
@@ -0,0 +1,74 @@
+let fetch = require('node-fetch');
+let Converter = require("csvtojson");
+let Promise = require('bluebird');
+let _ = require("lodash");
+let tr = require('transliteration').transliterate;
+let readFile = Promise.promisify(require('fs').readFile);
+
+//const declarationsSuffix = '?format=json';
+//const googleSheetsLinksFileModel = {
+//    name: "name",
+//    link: "link"
+//};
+const judgeModel = {
+    'e2013': '2013',
+    'source2013': 'Оригінал 2013',
+    'e2014': '2014',
+    'source2014': 'Оригінал 2014',
+    'e2015': '2015',
+    'source2015': 'Оригінал 2015',
+    'number': 'number',
+    'SNP': 'ПІБ',
+    'position': 'Посада',
+    'info': 'Примітки'
+};
+
+function prepareJudgeName (name) {
+    return tr(name).split(' ').join('-')
+}
+
+function readGoogleSheetsBunchFromFile(filePath) {
+    return readFile(filePath, 'utf8')
+        .then(function (data) {
+            return Promise.reduce(JSON.parse(data), function (judges, kyivCourtsList) {
+                return fetch(kyivCourtsList.link)
+                    .then(response => response.text())
+                    .then(function (csv) {
+                        let converter = new Converter.Converter({
+                            workerNum: 4
+                        });
+                        return new Promise(function (resolve, reject) {
+                            return converter.fromString(csv, function (error, json) {
+                                if (error) {
+                                    reject(error);
+                                }
+                                resolve(judges.concat(json));
+                            });
+                        });
+                    });
+            }, []);
+        });
+
+}
+
+function readDirFileNames() {
+    return readGoogleSheetsBunchFromFile("./source/kyiv.json")
+        .then(function (judges) {
+            return _.filter(judges, function (j) {
+                return !!j[judgeModel.e2015] || !!j[judgeModel.e2014] || !!j[judgeModel.e2013];
+            })
+        })
+        .then(function (judges) {
+            judges.forEach(function (judge) {
+                judge.name = prepareJudgeName(judge[judgeModel.SNP]);
+            });
+            return judges;
+        })
+        .then(function (judges) {
+
+        })
+        .then(r => console.log(r.length))
+        .catch(console.log)
+}
+
+readDirFileNames();
