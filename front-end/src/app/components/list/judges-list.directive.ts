904d1ce8bc62e23f4b6aa62a0624e557c528776c
Ilya Litvinov <ilya.litvinov@dataart.com>
2016-07-05 12:38:01 +0300

    Added limit to grid

--- front-end/src/app/components/list/judges-list.directive.ts
+++ front-end/src/app/components/list/judges-list.directive.ts
@@ -1,53 +1,98 @@
+import * as _ from 'lodash';
 /** @ngInject */
+interface IScope extends angular.IScope {
+	data: any,
+	vm: JudgesListController
+}
 export function list(): angular.IDirective {
 
-  return {
-    restrict: 'E',
-    scope: {
-      data: '='
-    },
-    templateUrl: 'app/components/list/judges-list.view.html',
-    controller: JudgesListController,
-    controllerAs: 'vm',
-    bindToController: true
-  };
+	return {
+		restrict: 'E',
+		scope: {
+			data: '='
+		},
+		templateUrl: 'app/components/list/judges-list.view.html',
+		controller: JudgesListController,
+		controllerAs: 'vm',
+		bindToController: true,
+		link: (scope: IScope, element: angular.IAugmentedJQuery, attrs: angular.IAttributes) => {
+			const table = element.find('.table-wrapper'),
+				th = table.find('th');
+
+			th.on('click', function (e) {
+				const target = this;
+				const isReversed = !Boolean(angular.element(target).attr('data-reversed'));
+				const sortKey = angular.element(target).attr('data-sort');
+
+				scope.vm.changeOrder(sortKey, isReversed);
+				if(isReversed) {
+					angular.element(target).attr('data-reversed', "true");
+				} else {
+					angular.element(target).removeAttr('data-reversed');
+				}
+			})
+		}
+	};
 
 }
 const DISPLAYING_LENGTH: number = 25;
 /** @ngInject */
-
 export class JudgesListController {
-  data: any;
+	data: any;
 
-  allJudges: any[];
-  dtColumnDefs: any[];
-  dtOptions: any;
-  dictionary: any;
+	allJudges: any[];
+	dtColumnDefs: any[];
+	dtOptions: any;
+	dictionary: any;
+	limit: number;
+	skiped: number;
 
-  private _state: any;
-  private _detailsUrl: string;
+	private _state: any;
+	private _detailsUrl: string;
 
 
-  constructor(DTColumnDefBuilder: any, $scope: angular.IScope, $state: any, urls: any) {
-    this._detailsUrl = urls.details;
-    this._state = $state;
+	constructor(DTColumnDefBuilder: any, $scope: angular.IScope, $state: any, urls: any) {
+		this._detailsUrl = urls.details;
+		this._state = $state;
+		this.limit = DISPLAYING_LENGTH;
+		this.skiped = 0;
 
-    this.dtOptions = {
-      paginationType: 'full_numbers',
-      displayLength: DISPLAYING_LENGTH
-    };
+		const listener = $scope.$watch(() => {
+			return this.data;
+		}, (newVal) => {
+			if (newVal.length !== 0) {
+				this.data = newVal;
+				this.toOrder('k', false);
+				listener();
+			}
+		})
+	}
 
-    this.dtColumnDefs = [
-      DTColumnDefBuilder.newColumnDef(0),
-      DTColumnDefBuilder.newColumnDef(1),
-      DTColumnDefBuilder.newColumnDef(2)
-    ];
+	toDetails(key: string) {
+		console.log('Before reload');
+		this._state.go('details', {key});
+	}
 
-  }
+	toOrder(sortingKey, isReversed) {
+		this.data.sort((prev, next) => {
+			if (prev[sortingKey] > next[sortingKey]) {
+				return 1;
+			}
+			if (prev[sortingKey] < next[sortingKey]) {
+				return -1;
+			}
 
-  toDetails(key: string) {
-    console.log('Before reload');
-    this._state.go('details', {key});
-  }
+			return 0;
+		});
+		if (isReversed) {
+			this.data.reverse()
+		}
 
+	}
+
+	changeOrder(sortingKey, isReversed) {
+		this.toOrder(sortingKey, isReversed);
+	}
 }
+
+
