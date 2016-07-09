17b68637ebae387a97d2cd9b7608c1b00a88463c
Maksym Popenko <max@cemoe.com>
2016-07-09 02:10:15 +0200

    Working on basic analytics

--- scraper/analytics.js
+++ scraper/analytics.js
@@ -31,16 +31,60 @@ var getIndex = {
         if (isEmptiness(judge)) {
             return result;
         }
+        var declaration2014 = get2014Declaration(judge);
+        if (!declaration2014) {
+            return result;
+        }
+        if(!_.has(declaration2014,"estate.23") || _.isNull(_.get(declaration2014,"estate.23"))){
+            return result;
+        }
+
+        result = _.reduce(declaration2014.estate["23"], function (sum, land) {
+            var space = _.toNumber(land.space);
+
+            if (_.lowerCase(land.space_units) === "га") {
+                space *= 10000;
+            }
+            //if (_.lowerCase(land.space_units) !== "га" || _.lowerCase(land.space_units) !== "м2" || land.space_units != "м²") {
+            //    throw new Error("space unit " + land.space_units);
+            //}
+
+            return sum + space;
+        }, result);
 
         return result;
     },
 
     commonFamilyIncome: function commonFamilyIncome(judge) {
+        var result = 0;
 
+        if (isEmptiness(judge)) {
+            return result;
+        }
+        var declaration2014 = get2014Declaration(judge);
+        if (!declaration2014 || !declaration2014.income["5"]) {
+            return result;
+        }
+
+        return result + _.toNumber(declaration2014.income["5"].value) + _.toNumber(declaration2014.income["5"].family);
     },
 
     houseArea: function houseArea(judge) {
+        var result = 0;
 
+        if (isEmptiness(judge)) {
+            return result;
+        }
+        var declaration2014 = get2014Declaration(judge);
+        if (!declaration2014) {
+            return result;
+        }
+
+        _.reduce(declaration2014.estate, function (sum, land) {
+            return sum + _.toNumber(land.space);
+        }, result);
+
+        return result;
     }
 
 };
@@ -67,7 +111,11 @@ function loadJudges(judges) {
     return Promise.reduce(judges, function (_judges, judgeData) {
         return loadJudge(judgeData)
             .then(function (judge) {
-                console.log(judge)
+                //judge.landOwnerIndex = getIndex.landownerByLands(judge);
+                judge.incomeIndex = getIndex.commonFamilyIncome(judge);
+                //judge.houseIndex = getIndex.houseArea(judge);
+
+                retur judgen
             })
     }, []);
 }
@@ -79,4 +127,10 @@ function loadJudge(judge) {
         .catch(function (e) {
             throw new Error(e.message);
         })
+}
+
+function get2014Declaration (judge) {
+    return _.filter(judge.declarations, function (declaration) {
+        return _.get(declaration, "intro.declaration_year") === "2014" || _.get(declaration, "intro.declaration_year") == 2014;
+    })[0]
 }
