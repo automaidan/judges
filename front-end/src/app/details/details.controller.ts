80ace9239b1460105b8d559b3cdebb003a683341
Ilya Litvinov <ilya.litvinov@dataart.com>
2016-07-12 19:34:27 +0300

    Added estate table

--- front-end/src/app/details/details.controller.ts
+++ front-end/src/app/details/details.controller.ts
@@ -1,5 +1,21 @@
 import * as _ from 'lodash';
+interface ITable {
+	flats: Object
+}
 
+const TABLE_MODEL = {
+	head: {
+		title: null,
+		years: []
+	},
+	table: {}
+};
+const countTotal = (arr, field) => {
+	return arr.reduce((res, item) => {
+		res += item[field].replace(',', '.') && parseFloat(item[field].replace(',', '.'));
+		return res;
+	}, 0)
+};
 export class DetailsController {
 	declarations: any[];
 	$scope: angular.IScope;
@@ -22,6 +38,7 @@ export class DetailsController {
 	getDetails(key: string) {
 		return this._api.getOne(key).then((data: any) => {
 			this.data = data;
+			console.log(data);
 			this.incomeShown = this.hasIncomes();
 			this.$scope.$apply();
 
@@ -29,26 +46,83 @@ export class DetailsController {
 	}
 
 	countInComes() {
-		const tableModel = {
-			head: {
-				title: 'Cтатки',
-				years: []
+		const tableModel = angular.copy(TABLE_MODEL);
+
+		tableModel.table = {
+			own:  {
+				title: 'Дохід',
+				value: []
 			},
-			table: {
-				own:  {
-					title: 'Дохід',
-					value: []
-				},
-				family: {
-					title: 'Дохід сім\'ї',
-					value: []
-				}
+			family: {
+				title: 'Дохід сім\'ї',
+				value: []
 			}
 		};
 
 		this.data.declarations.forEach((item) => {
-			tableModel.table.family.value.push(item.income[5].family.replace(',', '.') && parseFloat(item.income[5].family.replace(',', '.')));
-			tableModel.table.own.value.push(item.income[5].value.replace(',', '.') && parseFloat(item.income[5].value.replace(',', '.')));
+			tableModel.table.family.value.push(
+				item.income[5].family.replace(',', '.')
+				&& (parseFloat(item.income[5].family.replace(',', '.')) + 'грн')
+			);
+
+			tableModel.table.own.value.push(
+				item.income[5].value.replace(',', '.')
+				&& (parseFloat(item.income[5].value.replace(',', '.')) + 'грн')
+			);
+			tableModel.head.title = "Статки";
+			tableModel.head.years.push(item.intro.declaration_year);
+		}, []);
+
+		return tableModel;
+	}
+
+	countRealEstate () {
+		const tableModel = angular.copy(TABLE_MODEL);
+
+		tableModel.table = {
+			flats:  {
+				title: 'Загальна площа власних квартир',
+				value: []
+			},
+			flatsFamily: {
+				title: 'Загальна площа квартир родини',
+				value: []
+			},
+			cottages: {
+				title: 'Загальна площа житлових будинків',
+				value: []
+			},
+			cottagesFamily: {
+				title: 'Загальна площа житлових будинків родини',
+				value: []
+			},
+			parcels: {
+				title: 'Земельні ділянки власні',
+				value: []
+			},
+			parcelsFamily: {
+				title: 'Земельні ділянки родини',
+				value: []
+			}
+
+		};
+
+		this.data.declarations.forEach((item) => {
+			const totalOwnFlats = item.estate[25] && (countTotal(item.estate[25], 'space') + item.estate[25][0].space_units),
+				totalFamilyFlats = item.estate[31] && (countTotal(item.estate[31], 'space') + item.estate[31][0].space_units),
+				totalCottages = item.estate[24] && (countTotal(item.estate[24], 'space') + item.estate[24][0].space_units),
+				totalFamilyCottages = item.estate[30] && (countTotal(item.estate[30], 'space') + item.estate[30][0].space_units),
+				totalParcel = item.estate[23] && (countTotal(item.estate[23], 'space') + item.estate[23][0].space_units),
+				totalFamilyParcels = item.estate[29] && (countTotal(item.estate[29], 'space') + item.estate[29][0].space_units);
+
+			tableModel.table.flats.value.push(totalOwnFlats);
+			tableModel.table.flatsFamily.value.push(totalFamilyFlats);
+			tableModel.table.cottages.value.push(totalCottages);
+			tableModel.table.cottagesFamily.value.push(totalFamilyCottages);
+			tableModel.table.parcels.value.push(totalParcel);
+			tableModel.table.parcelsFamily.value.push(totalFamilyParcels);
+
+			tableModel.head.title = 'Майно';
 			tableModel.head.years.push(item.intro.declaration_year);
 		}, []);
 
@@ -58,7 +132,8 @@ export class DetailsController {
 	filterData(key) {
 		let _this = this,
 			map = {
-				'income': () => {return _this.countInComes()}
+				'income': () => {return _this.countInComes()},
+				'realEstate': () => {return _this.countRealEstate()}
 			};
 
 		this.toRenderData(map[key]());
