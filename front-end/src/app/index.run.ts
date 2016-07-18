f2e4a765982dc8a5f21f26b7e67f9b5b2ef2f8c0
Maksym Popenko <max@cemoe.com>
2016-07-19 00:05:26 +0300

    Try to import typings google.analytics/ga

--- front-end/src/app/index.run.ts
+++ front-end/src/app/index.run.ts
@@ -1,8 +1,13 @@
 import { IStateRootScope } from './common/directives/state-detector-directive';
+// import {ga} from 'google.analytics/ga';
 /** @ngInject */
-export function runBlock($log: angular.ILogService, $state: any, $rootScope: IStateRootScope) {
+export function runBlock($log: angular.ILogService, $state: any, $rootScope: IStateRootScope, $window: angular.IWindowService) {
 	$rootScope.$on('$stateChangeStart', (e: any, toState: any) => {
 		$rootScope.isGradient = toState.name !== 'details';
 		$rootScope.currentState = toState.name;
 	});
+  $rootScope.$on('$stateChangeSuccess', (e: any, toState: any) => {
+    $window.ga('set', 'page', toState.url.replace(':key', $state.params.key));
+    ga('send', 'pageview');
+  });
 }
