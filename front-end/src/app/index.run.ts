8a602e83c658b78c1e9d3cff8989c5f9efd66d51
Ilya Litvinov <ilya.litvinov@dataart.com>
2016-07-19 14:07:23 +0300

    Fixed bug mozilla

--- front-end/src/app/index.run.ts
+++ front-end/src/app/index.run.ts
@@ -1,7 +1,10 @@
 import { IStateRootScope } from './common/directives/state-detector-directive';
+interface IWindow extends angular.IWindowService {
+	ga: any
+}
 // import {ga} from 'google.analytics/ga';
 /** @ngInject */
-export function runBlock($log: angular.ILogService, $state: any, $rootScope: IStateRootScope, $window: angular.IWindowService) {
+export function runBlock($log: angular.ILogService, $state: any, $rootScope: IStateRootScope, $window: IWindow) {
 	$rootScope.$on('$stateChangeStart', (e: any, toState: any) => {
 		$rootScope.isGradient = toState.name !== 'details';
 		$rootScope.currentState = toState.name;
