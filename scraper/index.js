5b5d8564020cbe1dd14d076e1164e5ce567a46fb
Maksym Popenko <max@cemoe.com>
2016-08-02 02:48:43 +0300

    Real duplicate workaround

--- scraper/index.js
+++ scraper/index.js
@@ -173,7 +173,11 @@ function getSearchLink(s) {
 function transliterateNames(judges) {
     console.log('transliterateNames');
     judges.forEach(function (judge) {
-        judge.key = _.toLower(transliterateName(judge[judgeModel.name]));
+        if ('мельник олександр михайлович' !== _.toLower(judge[judgeModel.name])) {
+            judge.key = _.toLower(transliterateName(judge[judgeModel.name]));
+        } else {
+            judge.key = _.toLower(transliterateName(judge[judgeModel.name] + " " + judge[judgeModel.region]));
+        }
     });
     return judges;
 }
