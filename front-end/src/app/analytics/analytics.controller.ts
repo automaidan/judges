b975e50be0629871a4571921db73f208400a80ff
Maksym Popenko <max@cemoe.com>
2016-08-23 02:24:47 +0300

    Rearrange code and remove debugger

--- front-end/src/app/analytics/analytics.controller.ts
+++ front-end/src/app/analytics/analytics.controller.ts
@@ -1,45 +1,45 @@
 import * as d3 from 'd3';
 
-import { IDropDownOption } from '../common/interfaces';
-import { IDropDownList } from '../common/interfaces';
+import {IDropDownOption} from '../common/interfaces';
+import {IDropDownList} from '../common/interfaces';
 
 interface IFilters {
-	year: string;
-	region: string;
-	department: string;
-	statistic: string;
+    year: string;
+    region: string;
+    department: string;
+    statistic: string;
 }
 
 interface IAnalyticsController {
-	// getData(): void;
-	addFilter(option: IDropDownOption, filter: string): void;
+    // getData(): void;
+    addFilter(option: IDropDownOption, filter: string): void;
 }
 
 let context: any = null;
 
 class AnalyticsController implements IAnalyticsController {
-	public units: string;
-	public data: any[];
-	public allYears: IDropDownList = [
+    public units: string;
+    public data: any[];
+    public allYears: IDropDownList = [
         {
             title: '2015',
             key: '2015'
         },
-		{
-			title: '2014',
-			key: '2014'
-		},
+        {
+            title: '2014',
+            key: '2014'
+        },
         {
             title: '2013',
             key: '2013'
         }
-	];
+    ];
 
-	public statistic: IDropDownList = [
-		{
-			title: 'Найбільший дохід',
-			key: 'i'
-		},
+    public statistic: IDropDownList = [
+        {
+            title: 'Найбільший дохід',
+            key: 'i'
+        },
         {
             title: 'Найбільший дохід сім’ї',
             key: 'm'
@@ -52,10 +52,10 @@ class AnalyticsController implements IAnalyticsController {
             title: 'Найбільша кількість земельних ділянк',
             key: 'z'
         },
-		{
-			title: 'Найбільша площа домівок',
-			key: 'h'
-		},
+        {
+            title: 'Найбільша площа домівок',
+            key: 'h'
+        },
         {
             title: 'Найбільша кількість домівок',
             key: 'e'
@@ -84,108 +84,105 @@ class AnalyticsController implements IAnalyticsController {
             title: 'Найбільша кількість розглянутих справ',
             key: 'w'
         }
-	];
+    ];
 
-	public allRegions: IDropDownList;
-	// public filtersByDepartments: IDropDownList;
-	public filterByIncomes: any = [];
-	public filtersApplied: any = false;
-	public availableDepartments: any;
+    public allRegions: IDropDownList;
+    // public filtersByDepartments: IDropDownList;
+    public filterByIncomes: any = [];
+    public filtersApplied: any = false;
+    public availableDepartments: any;
 
-	private $scope: any;
-	private _api: any;
-	private filters: IFilters = {
-		year: '',
-		region: '',
-		department: '',
-		statistic: ''
-	};
-	private originalData: any[];
-	private $filter: any;
-	private originalDepartments: any;
+    private $scope: any;
+    private _api: any;
+    private filters: IFilters = {
+        year: '2015',
+        region: '',
+        department: '',
+        statistic: 'i'
+    };
+    private originalData: any[];
+    private $filter: any;
+    private originalDepartments: any;
 
 
-	/* @ngInject */
-	constructor(Api: any, $scope: angular.IScope, $filter: angular.IFilterProvider) {
-		context = this;
+    /* @ngInject */
+    constructor(Api: any, $scope: angular.IScope, $filter: angular.IFilterProvider) {
+        context = this;
 
-		this._api = Api;
-		this.$scope = $scope;
+        this._api = Api;
+        this.$scope = $scope;
 
-		this._api.getJudgesList()
-			.then(resp => {
-				this.data = resp;
-				this.originalData = angular.copy(resp);
-				return this._api.getDepartments();
-			})
-			.then((res) => {
-				this.originalDepartments = res;
-				this.availableDepartments = this.reduceDepartments(this.originalDepartments );
-				debugger;
-				return this._api.getRegions();
-			})
-			.then(resp => {
-				debugger;
-				this.allRegions = resp;
-				$scope.$applyAsync();
-			});
+        this._api.getJudgesList()
+            .then(resp => {
+                this.data = resp;
+                this.originalData = angular.copy(resp);
+                return this._api.getDepartments();
+            })
+            .then((res) => {
+                this.originalDepartments = res;
+                this.availableDepartments = this.reduceDepartments(this.originalDepartments);
+                // debugger;
+                return this._api.getRegions();
+            })
+            .then(resp => {
+                // debugger;
+                this.allRegions = resp;
+                $scope.$applyAsync();
+            });
 
-	}
+    }
 
-	/** @ngInject */
+    /** @ngInject */
 
-	addFilter(option: IDropDownOption, filter: string) {
-		if(filter === 'region') {
-			context.availableDepartments = context.filterDepartmentByRegion(context.originalDepartments, option.key);
-			debugger;
-			context.$scope.$evalAsync();
-		}
-		context.filters[filter] = option.key;
+    addFilter(option: IDropDownOption, filter: string) {
+        if (filter === 'region') {
+            context.availableDepartments = context.filterDepartmentByRegion(context.originalDepartments, option.key);
+            context.$scope.$evalAsync();
+        }
+        context.filters[filter] = option.key;
         context.filterApply();
-	}
+    }
 
-	filterApply() {
-		this.data = this.originalData;
+    filterApply() {
+        this.data = this.originalData;
 
-		if (this.filters.year) {
-			this.data = this.$filter('filterByYear')(this.data, parseInt(this.filters.year, 10));
-		}
-		if (this.filters.region) {
-			this.data = this.$filter('filterByField')(this.data, this.filters.region, 'r');
-		}
-		if (this.filters.department) {
-			this.data = this.$filter('filterByField')(this.data, this.filters.department, 'd');
-		}
-		// if (this.filters.department) {
-		// 	this.data = this.$filter('filterByField')(this.data, this.filters.department, 'd');
-		// }
-		if (this.filters.statistic) {
-			this.data = this.$filter('filterByAnalyticsField')(this.data, this.filters.statistic);
-			this.units = (this.filters.statistic === 'i') ? 'грн' : '';
-		}
-	}
+        if (this.filters.year) {
+            this.data = this.$filter('filterByYear')(this.data, parseInt(this.filters.year, 10));
+        }
+        if (this.filters.region) {
+            this.data = this.$filter('filterByField')(this.data, this.filters.region, 'r');
+        }
+        if (this.filters.department) {
+            this.data = this.$filter('filterByField')(this.data, this.filters.department, 'd');
+        }
+        // if (this.filters.department) {
+        // 	this.data = this.$filter('filterByField')(this.data, this.filters.department, 'd');
+        // }
+        if (this.filters.statistic) {
+            this.data = this.$filter('filterByAnalyticsField')(this.data, this.filters.statistic);
+            this.units = (this.filters.statistic === 'i') ? 'грн' : '';
+        }
+    }
 
-	private filterDepartmentByRegion (departmentRegionsObj, region) {
-		let availableDepartments = [];
+    private filterDepartmentByRegion(departmentRegionsObj, region) {
+        let availableDepartments = [];
 
-		if(region) {
-			availableDepartments = departmentRegionsObj[region];
-		} else {
-			availableDepartments = this.reduceDepartments(departmentRegionsObj);
-		}
-		debugger;
-		return availableDepartments
-	}
+        if (region) {
+            availableDepartments = departmentRegionsObj[region];
+        } else {
+            availableDepartments = this.reduceDepartments(departmentRegionsObj);
+        }
+        return availableDepartments;
+    }
 
-	private reduceDepartments (obj) {
-		let reduced = [];
+    private reduceDepartments(obj) {
+        let reduced = [];
 
-		for (let key in obj) {
-			reduced = reduced.concat(obj[key]);
-		}
-		debugger;
-		return reduced;
-	}
+        for (let key in obj) {
+            reduced = reduced.concat(obj[key]);
+        }
+        return reduced;
+    }
 }
 
-export { AnalyticsController };
+export {AnalyticsController};
