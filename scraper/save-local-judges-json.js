90dda043b24d847bc332e89994852f81591f2978
Maksym Popenko <popenkomaksim@gmail.com>
2016-10-06 15:49:57 +0300

    calculate how much judges with photo

--- scraper/save-local-judges-json.js
+++ scraper/save-local-judges-json.js
@@ -1,10 +1,16 @@
 "use strict";
+let _ = require('lodash');
 let Promise = require('bluebird');
 let writeFile = Promise.promisify(require('fs').writeFile);
 
 const input = require("./input");
 
 module.exports = function saveLocalJudgesJSON(judges) {
+
+    console.log("Кількість фото " + JSON.stringify(_.countBy(judges, function (judge) {
+            return !!judge['Фото'];
+        })));
+
     return writeFile(input.cachedJudges, JSON.stringify(judges))
         .then(() => judges);
 };
