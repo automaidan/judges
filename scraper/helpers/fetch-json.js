8f9f007fcc3fa133e18b715c6c030012910e5a94
Maksym Popenko <popenkomaksim@gmail.com>
2016-11-27 01:41:41 +0200

    Update fetch json It gotta keep trying, and never give up!

--- scraper/helpers/fetch-json.js
+++ scraper/helpers/fetch-json.js
@@ -4,4 +4,9 @@ module.exports = function searchDeclaration(link) {
     return fetch(link)
         .then(response => response.text())
         .then(data => JSON.parse(data))
+        .catch((err) => {
+            console.log(err);
+            console.log("...But, I gotta keep trying, and never give up!");
+            return module.exports(link);
+        })
 };
