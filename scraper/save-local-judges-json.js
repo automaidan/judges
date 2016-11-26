c82d96429b842b317808ef848ab07e055f2991a8
Maksym Popenko <popenkomaksim@gmail.com>
2016-11-26 23:42:37 +0200

    Save only freshly scraped judges

--- scraper/save-local-judges-json.js
+++ scraper/save-local-judges-json.js
@@ -6,6 +6,10 @@ let writeFile = Promise.promisify(require('fs').writeFile);
 const input = require("./input");
 
 module.exports = function saveLocalJudgesJSON(judges) {
+    if (process.env.LOCAL_JUDGES_JSON) {
+        return judges;
+    }
+
     console.log("Save fetched judges list locally.");
     return writeFile(input.cachedJudges, JSON.stringify(judges))
         .then(() => judges);
