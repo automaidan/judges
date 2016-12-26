c441c06458723f839363f411f78a872ae6863f8a
Maksym Popenko <popenkomaksim@gmail.com>
2016-12-26 10:54:04 +0200

    Reduce lint error amount

--- front-end/src/app/about/about.controller.ts
+++ front-end/src/app/about/about.controller.ts
@@ -1,34 +1,33 @@
 interface IAboutController {
-  aboutText: string;
-  getData(): Promise<any>;
+    aboutText: string;
+    getData(): Promise<any>;
 }
 
 class AboutController implements IAboutController {
-  aboutText: any;
-  aboutTitleAdditional: any;
-  aboutAdditional: any;
-  $scope: any;
+    aboutText: any;
+    aboutTitleAdditional: any;
+    aboutAdditional: any;
+    $scope: any;
 
-  private _api: any;
-  /* @ngInject */
+    private _api: any;
+    /* @ngInject */
 
 
-  constructor(Api: any, $scope: angular.IScope) {
-    console.log('Hello About');
-    this._api = Api;
-    this.$scope = $scope;
-    this.getData();
-  }
+    constructor(Api: any, $scope: angular.IScope) {
+        this._api = Api;
+        this.$scope = $scope;
+        this.getData();
+    }
 
-  /** @ngInject */
-  getData() {
-    return this._api.getTexts().then((res: any) => {
-      this.aboutText = res.about_project;
-      this.aboutTitleAdditional = res.about_project_title_additional;
-      this.aboutAdditional = res.about_project_text_additional;
-      this.$scope.$apply();
-    });
-  }
+    /** @ngInject */
+    getData() {
+        return this._api.getTexts().then((res: any) => {
+            this.aboutText = res.about_project;
+            this.aboutTitleAdditional = res.about_project_title_additional;
+            this.aboutAdditional = res.about_project_text_additional;
+            this.$scope.$apply();
+        });
+    }
 }
 
-export { AboutController };
+export {AboutController};
