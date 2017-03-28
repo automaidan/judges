ae3ebc5046c5c2183601bf850504e006a25a5c0f
Maksym Popenko <popenkomaksim@gmail.com>
2017-03-29 01:43:44 +0300

    Tests. getFlatAmount expected result in this case 0

--- scraper/test/public-api.nazk.gov.ua/analytics.js
+++ scraper/test/public-api.nazk.gov.ua/analytics.js
@@ -3,7 +3,7 @@ const analytics = require("../../providers/public-api.nazk.gov.ua/analytics");
 
 const oneFlatMultipleTimesDeclared = require("../../../public-api.nazk.gov.ua/582ada10-1bfe-4d7b-9b59-bfd1d92f546d.json");
 test("public-api.nazk.gov.ua analytics getFlatAmount check", function (t) {
-    t.equal(analytics.getFlatAmount(oneFlatMultipleTimesDeclared), 1);
+    t.equal(analytics.getFlatAmount(oneFlatMultipleTimesDeclared), 0);
     t.end();
 });
 
