0b0b9668ac0db7c1a27c52a6685c63719319f1ee
Maksym Popenko <popenkomaksim@gmail.com>
2017-06-19 13:23:09 +0300

    Config fix

--- scraper/config.js
+++ scraper/config.js
@@ -1,11 +1,12 @@
 const nconf = require("nconf");
 const config = nconf
+    .use('memory')
     .argv()
     .env()
     .defaults(require("./config.default.json"));
 
-config.set(config.get("LOCAL_JUDGES_JSON") === true || config.get("LOCAL_JUDGES_JSON") === "true");
-config.set(parseInt(config.get("SCRAPPER_SPEED"), 10));
-config.set(parseInt(config.get("PERSONS_LIMIT"), 10));
+config.set("LOCAL_JUDGES_JSON", config.get("LOCAL_JUDGES_JSON") === true || config.get("LOCAL_JUDGES_JSON") === "true");
+config.set("SCRAPPER_SPEED", parseInt(config.get("SCRAPPER_SPEED"), 10));
+config.set("PERSONS_LIMIT", parseInt(config.get("PERSONS_LIMIT"), 10));
 
 module.exports = config;
