02ab7f1c95dd0c0fd564c3eaa11de0c4c602839a
Maksym Popenko <popenkomaksim@gmail.com>
2017-06-20 01:36:57 +0300

    Truncate unused code

--- front-end/src/app/list/list.controller.ts
+++ front-end/src/app/list/list.controller.ts
@@ -1,22 +1,8 @@
-// import { IApi } from '../common/services/api.service';
-
-// const DISPLAYING_LENGTH: number = 25;
-
-interface IJudgesListInterface {
-    allJudges: any[];
-    getData(): void;
-}
-
 export class JudgesListController {
     private _api: any;
 
     /* @ngInject */
     constructor(Api: any) {
         this._api = Api;
-    }
-
-    /** @ngInject */
-    getData() {
-        console.log('judges loaded');
     }
 }
