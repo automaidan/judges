e896e86f426b0991561b0ba456372d97dcd106b0
Maksym Popenko <popenkomaksim@gmail.com>
2018-03-12 10:27:42 +0200

    Update helper toolchain

--- scraper/helpers/to-safest-number.js
+++ scraper/helpers/to-safest-number.js
@@ -1,13 +1,14 @@
-'use strict';
 const _ = require('lodash');
+
 module.exports = function toSafestNumber(string) {
   return _.chain(string)
     .replace(',', '.')
     .thru(s => parseFloat(s))
-    .thru(s => {
+    .thru((s) => {
       if (!_.isNaN(s)) {
         return _.round(s, 2);
       }
+      return undefined;
     })
     .value();
 };
