f68022ceb0602f4c3232f556000840494f36d358
Maksym Popenko <popenkomaksim@gmail.com>
2018-03-12 11:29:25 +0200

    Update toUAH helper

--- scraper/helpers/to-uah.js
+++ scraper/helpers/to-uah.js
@@ -1,18 +1,18 @@
-'use strict';
 const rates = require('./exchange-rates').rates;
-module.exports = function toUAH(sum, currency) {
-  if (sum === '' || typeof sum === 'undefined') {
+
+module.exports = function toUAH(rawSum, currency) {
+  if (rawSum === '' || typeof rawSum === 'undefined') {
     return 0;
   }
-  sum = parseFloat(sum.replace(',', '.'));
-  if (currency === 'грн' || currency === 'грн.' || currency === 'UAH' || currency === '' || typeof currency === 'undefined') {
+  const sum = typeof currency === 'string' ? parseFloat(rawSum.replace(',', '.')) : rawSum;
+  if (['грн', 'грн.', 'UAH', ''].includes(currency) || typeof currency === 'undefined') {
     return sum;
   } else if (currency === 'OTHER') {
-
     // Карбованці
     return 0;
   } else if (rates[currency]) {
     return sum * rates[currency];
   }
   console.log(`${currency} is missing`);
+  return undefined;
 };
