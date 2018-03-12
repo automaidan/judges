83fb62f59f237bcdfe0b6e719aa83be8400d8a4f
Maksym Popenko <popenkomaksim@gmail.com>
2018-03-12 11:00:39 +0200

    Update helper toolchain

--- scraper/helpers/to-square-meters.js
+++ scraper/helpers/to-square-meters.js
@@ -1,12 +1,8 @@
-function isFloat(n) {
-  return Number(n) === n && n % 1 !== 0;
-}
-
 module.exports = function toSquareMeters(rawSpace, spaceUnits) {
   if (rawSpace === '' || typeof rawSpace === 'undefined') {
     return 0;
   }
-  const space = isFloat(rawSpace) ? parseFloat(rawSpace.replace(',', '.')) : rawSpace;
+  const space = typeof rawSpace === 'string' ? parseFloat(rawSpace.replace(',', '.')) : rawSpace;
 
   if (spaceUnits === 'м²' || spaceUnits === '' || typeof spaceUnits === 'undefined') {
     return space;
