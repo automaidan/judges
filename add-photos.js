65c781eba04c89b2739f5aeb93a0d0cfef39ea6e
Maksym Popenko <3439938+popenkomaksim@users.noreply.github.com>
2023-07-06 14:19:18 +0300

    Update photos

--- add-photos.js
+++ add-photos.js
@@ -1,22 +1,46 @@
 //requiring path and fs modules
-const path = require('path');
-const fs = require('fs');
-const fspromises = require('fs/promises');
-//joining path of directory 
-const directoryPath = path.join(__dirname, 'photos');
+const path = require("path");
+const fs = require("fs");
+const fspromises = require("fs/promises");
+//joining path of directory
+const directoryPath = path.join(__dirname, "photos");
 //passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
+  //handling error
+  if (err) {
+    return console.log("Unable to scan directory: " + err);
+  }
+  //listing all files using forEach
+  files.forEach(async function (file) {
+    // Do whatever you want to do with the file
+    const key = file.split(".")[0];
+    console.log(key);
+
+    if (!key) {
+      return;
+    }
+
+    // || key === "bodnar_serhii_bohdanovych" || key === "britanchyuk_volodimir_vasilovich"
+    let isExists;
+    try {
+        isExists = await fspromises.access(`./profiles/${key}.json`);
     } 
-    //listing all files using forEach
-    files.forEach(async function (file) {
-        // Do whatever you want to do with the file
-        console.log(file.split('.')[0]);
-        const data = await fspromises.readFile(`./profiles/${file.split('.')[0]}.json`, { encoding: 'utf8' });
-        const dataObj = JSON.parse(data);
-        dataObj["Фото"] = `https://prosud.info/photos/${file}`;
-        await fspromises.writeFile(`./profiles/${file.split('.')[0]}.json`, JSON.stringify(dataObj));
-    });
+    catch (e) {
+        isExists = false;
+    }
+    if (!isExists) {
+        return;
+    }
+
+    const data = await fspromises.readFile(
+      `./profiles/${key}.json`,
+      { encoding: "utf8" }
+    );
+    const dataObj = JSON.parse(data);
+    dataObj["Фото"] = `https://prosud.info/photos/${file}`;
+    await fspromises.writeFile(
+      `./profiles/${key}.json`,
+      JSON.stringify(dataObj)
+    );
+  });
 });
